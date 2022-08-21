4485a57382a33674e44e7cc85aa35a02512db346
sepatubenar <sepatubenar@gmail.com>
2022-08-21 15:19:30 +0700

    menambhakan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga maxime nobis aspernatur totam culpa! A porro alias officia reprehenderit aspernatur, corporis, ipsum maxime corrupti cumque omnis natus deleniti, temporibus quia.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
